d9a854e95772920b493f90b9bbcf4bfd923b43d0
Weggler <lucy.weggler@htw-berlin.de>
2026-08-31 19:16:59 +0200

    add functions to get characteristic mesh size h / extend ref table

--- convergence_timing/common.py
+++ convergence_timing/common.py
@@ -1,4 +1,19 @@
+from math import dist
+
 import pandas as pd
+
+
+def max_mesh_size(mesh):
+    """Return the maximum edge length of the surface mesh."""
+    points = mesh.ngmesh.Points()
+    h = 0.0
+
+    for element in mesh.ngmesh.Elements2D():
+        vertices = [vertex.nr for vertex in element.vertices]
+        for first, second in zip(vertices, vertices[1:] + vertices[:1]):
+            h = max(h, dist(tuple(points[first]), tuple(points[second])))
+
+    return h
 
 
 def append_results(results, csv_path):
